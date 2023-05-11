fa81ab16d98c693cf1ad4cfa790d8b873855bbd2
Abhiraj Shourya <abhirajshourya@gmail.com>
2023-05-12 02:31:46 +0530

    API: getSingleMovie and POST review by imdbId

--- src/main/java/dev/abhiraj/movies/MovieService.java
+++ src/main/java/dev/abhiraj/movies/MovieService.java
@@ -1,6 +1,7 @@
 package dev.abhiraj.movies;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,9 @@ public class MovieService {
 
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String id) {
+        return movieRepository.findMovieByImdbId(id);
     }
 }
